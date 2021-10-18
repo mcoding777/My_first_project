f0631f5cd247c61796433776f3776d423c3bc53b
misun <eating8@naver.com>
2021-10-18 23:43:49 +0900

    버튼에 js로 온클릭 적용!!!

---
+++ index.js
@@ -0,0 +1,11 @@
+function greeting() {
+    document.querySelector("#greeting h2").innerHTML = '안녕하세요! 새내기 개발자입니다 :)'
+}
+
+function framework() {
+    document.querySelector("#greeting h2").innerHTML = '제 프레임워크 기술입니다 :)'
+}
+
+function project() {
+    document.querySelector("#greeting h2").innerHTML = '제가 작업한 프로젝트입니다 :)'
+}
